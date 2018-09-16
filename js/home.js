086ee81b80b2d14e4ba8c14f2ed715fe9a49342a
hwhinowa <jejinoes1@gmail.com>
2018-09-16 18:34:50 +0900

    'sub_page_staff_create'

--- js/home.js
+++ js/home.js
@@ -1,9 +1,19 @@
-'use strict';
+// 'use strict';
 
 let intro = document.getElementById('intro');
+let header = document.getElementsByTagName('header')[0];
+let menu_box = document.getElementsByClassName('header_wrap')[0];
+let typing = document.getElementsByClassName('typing')[0];
+
 intro.onended = function(){
     intro.classList.add('fade_hide');
     setTimeout(function(){
-        intro.style.display = 'none';
-    },6000);
+        show();
+    },500);
+}
+
+function show(){
+    intro.style.display = 'none';
+    header.classList.add('show');
+    menu_box.classList.add('show');
 }
---
+++ js/staff.js
@@ -0,0 +1,5 @@
+let typing = document.getElementsByClassName('typing')[0];
+
+setTimeout(function () {
+    typing.style.opacity = 1;
+}, 500);
